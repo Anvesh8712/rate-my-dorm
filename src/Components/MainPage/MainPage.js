41df89d507432ef9474c512229d83bd1d9c1474a
Anvesh Laptop 2 <atvanvesh@gmail.com>
2023-02-14 09:35:05 -0600

    added new ideas

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -9,6 +9,7 @@ function MainPage() {
                 -{">"} sort ranking by feature - bathrooms, dorms, laundry location, 
                 distance to class, distance to food, social life, people, distance to rsd,
                 check in process, room layout, view out of room , etc
+                -{">"} Bathrooms, Comfort(cozy), Space, distance from class, distance to dining hall
                 -{">"} algorithm will assign a rating to it 
                 -{">"} averages the rating for multiple users 
                 -{">"} saves json file? or just rating? and averages them out
