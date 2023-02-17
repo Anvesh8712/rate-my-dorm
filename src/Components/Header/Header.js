33e825b9ede6c6bc7074cc8c69699990786054a7
Anvesh Laptop 2 <atvanvesh@gmail.com>
2023-02-17 11:25:08 -0600

    added picture some touches on header css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,16 +8,20 @@
         logo <br/>
         -{">"} RateMyDorm logo
  */
+import './Header.css'
+import React from 'react'
+import home_pic from './icons8-home-page-80.png'
+
 
 
 
 function Header() {
 
 return(
-    <div>
-        <p>
-        
-        </p>
+    <div className='Main'>
+        <img src={home_pic} alt='home icon'></img>
+        <h1 className='h1'>RATE MY DORM - UMASS</h1>
+        <h1 className='h1'>Contact Us</h1>
     </div>
 )
 }
